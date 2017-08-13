8fbc4d9b4ab1a6457668fc173728684f942596c1
Ohad Lutzky <ohad@lutzky.net>
2017-08-13 22:48:33 +0100

    All tests now pass
    They don't test much, but they don't error out either. Working on #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
 
+// TODO(lutzky): Do we really need to import and declare every module,
+// every pipe, everything altogether - in every component that uses it?
+import { WeekdayPipe } from './weekday.pipe';
+import { FromMinutesPipe } from './from-minutes.pipe';
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ScheduleViewerComponent } from './schedule-viewer/schedule-viewer.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-/*
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CatalogComponent,
+        ScheduleViewerComponent,
+        WeekdayPipe,
+        FromMinutesPipe
       ],
+      imports: [ HttpClientTestingModule ]
     }).compileComponents();
   }));
 
@@ -28,7 +39,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('TTime2');
   }));
 });
-*/
